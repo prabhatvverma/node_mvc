5e0114506fe605f70cef5b1942c855450752f6f4
prabahtvverma <prabhatvverma@gmail.com>
2023-02-22 12:08:30 +0530

    exporting data to any file from project

--- models/emplyDetails.js
+++ models/emplyDetails.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const employee = mysql.Schema({
 
+    employee_id:{
+        type:String,
+        required: true
+    },
+
     name: {
         type: String,
         required: true
@@ -8,5 +13,13 @@ const employee = mysql.Schema({
     email: {
         type: String,
         required: true
-    }
-})
+    },
+    mobile_no :{
+        type:Number,
+        required: true
+    },
+
+});
+
+const Employee = mongoose.model("Employee", "employee");
+module.exports= Employee;
